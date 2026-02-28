510c403c5b04ab0264cac48d15e328fb16fdcc3c
Thunderbotz9650 <thunderbotz089@gmail.com>
2026-02-28 12:32:51 -0500

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -7,7 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
+import frc.robot.subsystems.CANDriveSubsystem;
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
  * the TimedRobot documentation. If you change the name of this class or the package after creating
@@ -101,3 +101,4 @@ public class Robot extends TimedRobot {
   @Override
   public void simulationPeriodic() {}
 }
+
